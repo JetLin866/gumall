374e6d793c049f2fbf15bf006ff158cc538ecd4b
JetLin866 <JetLin866@163.com>
2022-03-14 10:07:45 +0800

    重写AttrValueWithSkuIdVo

---
+++ gulimall/gulimall-product/src/main/java/com/xunqi/gulimall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,15 @@
+package com.xunqi.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author cylin
+ * @date 2022/3/14 10:06
+ */
+
+@Data
+public class AttrValueWithSkuIdVo {
+    private String attrValue;
+
+    private String skuIds;
+}
